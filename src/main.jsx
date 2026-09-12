0fce03c4c567fc35d44a6c6b3c0987a12e3b8d47
erikadkhar4-dev <erikadkhar4@gmail.com>
2026-09-12 06:57:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ClientFinderAI from "../client-finder-ai";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ClientFinderAI />
-  </React.StrictMode>
-);
+const path = window.location.pathname;
+
+if (path === "/") {
+  window.location.replace("/clientai-landing.html");
+} else {
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <ClientFinderAI />
+    </React.StrictMode>
+  );
+}
