f1ffcc8674b8ef36fb609b879090aff0f5c201d8
erikadkhar4-dev <erikadkhar4@gmail.com>
2026-09-13 12:01:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, open: true },
-  build: { outDir: 'dist', sourcemap: false }
+
+  server: {
+    port: 3000,
+    open: true
+  },
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        app: resolve(process.cwd(), 'app.html')
+      }
+    }
+  }
 })
